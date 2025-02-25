1cf47496ffb03ce24d84a111240db133a977344c
Thomas Yang <yhz2004@connect.hku.hk>
2025-02-25 14:12:01 -0500

    Adding more comments in marching cube utils

--- utils/mcube_utils.py
+++ utils/mcube_utils.py
@@ -14,17 +14,29 @@ def marching_cubes_with_contraction(
     inv_contraction=None,
     max_range=32.0,
 ):
-
+    """ Marching cubes algorithm with contraction and clipping
+    Args:
+        sdf: signed distance function
+        resolution: resolution of the grid
+        bounding_box_min: minimum corner of the bounding box
+        bounding_box_max: maximum corner of the bounding box
+        return_mesh: return trimesh object
+        level: level set value
+        simplify_mesh: simplify the mesh
+        inv_contraction: inverse contraction function
+        max_range: maximum range of the points
+    
+    """
     assert resolution % 512 == 0
 
     resN = resolution
-    cropN = 512
+    cropN = 512 # chunk size
     level = 0
     N = resN // cropN
 
     grid_min = bounding_box_min
     grid_max = bounding_box_max
-    xs = np.linspace(grid_min[0], grid_max[0], N + 1)
+    xs = np.linspace(grid_min[0], grid_max[0], N + 1) # create partitioning coordinates for chunk boundaries
     ys = np.linspace(grid_min[1], grid_max[1], N + 1)
     zs = np.linspace(grid_min[2], grid_max[2], N + 1)
 
@@ -37,10 +49,12 @@ def marching_cubes_with_contraction(
                 y_min, y_max = ys[j], ys[j + 1]
                 z_min, z_max = zs[k], zs[k + 1]
 
+                # local grid coordinates
                 x = torch.linspace(x_min, x_max, cropN).cuda()
                 y = torch.linspace(y_min, y_max, cropN).cuda()
                 z = torch.linspace(z_min, z_max, cropN).cuda()
 
+                # Generate 3D grid points
                 xx, yy, zz = torch.meshgrid(x, y, z, indexing="ij")
                 points = torch.tensor(torch.vstack([xx.ravel(), yy.ravel(), zz.ravel()]).T, dtype=torch.float).cuda()
 
@@ -63,19 +77,21 @@ def marching_cubes_with_contraction(
                         volume=z.reshape(cropN, cropN, cropN),
                         level=level,
                         spacing=(
-                            (x_max - x_min) / (cropN - 1),
+                            (x_max - x_min) / (cropN - 1), # voxel size
                             (y_max - y_min) / (cropN - 1),
                             (z_max - z_min) / (cropN - 1),
                         ),
                     )
+                    # Transform local coordinates to global space
                     verts = verts + np.array([x_min, y_min, z_min])
+                    # Create chunk mesh
                     meshcrop = trimesh.Trimesh(verts, faces, normals)
                     meshes.append(meshcrop)
                 
                 print("finished one block")
 
     combined = trimesh.util.concatenate(meshes)
-    combined.merge_vertices(digits_vertex=6)
+    combined.merge_vertices(digits_vertex=6) # specify precision
 
     # inverse contraction and clipping the points range
     if inv_contraction is not None:
